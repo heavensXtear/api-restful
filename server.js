0ee681568c1e1a23fdbfc092fd3e585d5137f76f
alexis <alexisdebons@hotmail.fr>
2018-11-30 10:32:46 +0100

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 
+app.get('/listUsers',function(req,res){
+  fs.readfile(_dirname + "/" + "users.json", 'utf8', function (err, data){
+    console.log(data);
+    res.send(data);
+  });
+})
+
+
 let server = app.listen(8081,function(){
   let host = server.address().address
   let port = server.address().port
